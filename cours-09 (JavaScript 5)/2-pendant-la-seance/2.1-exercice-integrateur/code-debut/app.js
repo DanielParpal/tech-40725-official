0a274af94542e7917c33f76a551de261eb967f6a
Daniel Parpal <daniel.parpal@hotmail.com>
2022-11-02 21:02:49 -0400

    Préparation ex intégrateur cours 9

---
+++ cours-09 (JavaScript 5)/2-pendant-la-seance/2.1-exercice-integrateur/code-debut/app.js
@@ -0,0 +1,40 @@
+// 1. Modélisation des données
+var tailles = [5, 6, 7, 8, 9, 10];
+
+var botteA = {
+	marque: 'UGG',
+	nom: 'CLASSIC MINI PLATFORM Women',
+	imageSrc: 'images/ugg-classic-mini-platform.jpeg',
+	imageAlt: 'Ugg classic mini platform',
+	taillesDispo: [5, 8]
+};
+
+var botteB = {
+	marque: 'UGG',
+	nom: 'NEUMEL PLATFORM Women',
+	imageSrc: 'images/ugg-neumel-platform.jpeg',
+	imageAlt: 'Ugg neumel platform',
+	taillesDispo: [5, 6, 8, 10]
+};
+
+var botteC = {
+	marque: 'UGG',
+	nom: 'CLASSIC MINI II Women',
+	imageSrc: 'images/ugg-classic-mini-ii.jpeg',
+	imageAlt: 'Ugg classic mini ii',
+	taillesDispo: [7, 8, 9, 10]
+};
+
+var botteD = {
+	marque: 'UGG',
+	nom: 'CLASSIC ULTRA MINI Women',
+	imageSrc: 'images/ugg-classic-ultra-mini.jpeg',
+	imageAlt: 'Ugg classic ultra mini',
+	taillesDispo: [6, 9]
+};
+
+var bottes = [
+botteA, botteB, botteC, botteD,
+];
+
+// Votre code viendra ci-dessous
